f6bfd44d3bd86f3478c9ad1c85879e67380fbd15
inga-dis <ingadis28@gmail.com>
2021-04-08 16:46:18 +0000

    instructions

--- mixup.js
+++ mixup.js
@@ -368,14 +368,18 @@ const menu = document.getElementById('menu');
 const highscoreButton = document.getElementById('highscoreButton');
 const highscoreMenu = document.getElementById('highscore');
 const back = document.getElementById('back');
+const instructionsback = document.getElementById('instructionsback');
 
-// instructionButton.addEventListener('click', () => {
-//     if (instructionsMenu.style.display === 'none') {
-//         instructionsMenu.style.display = 'flex';
-//     } else {
-//         instructionsMenu.style.display = 'none';
-//     }
-// });
+instructionButton.addEventListener('click', () => {
+     if (instructionsMenu.style.display === 'none') {
+         instructionsMenu.style.display = 'flex';
+     } else {
+        instructionsMenu.style.display = 'none';
+    }
+});
+instructionsback.addEventListener('click', () => {
+    instructionsMenu.style.display = 'none';
+})
 highscoreButton.addEventListener('click', () => {
     if (highscoreMenu.style.display === 'none') {
         highscoreMenu.style.display = 'flex';
@@ -386,7 +390,6 @@ highscoreButton.addEventListener('click', () => {
 });
 back.addEventListener('click', () => {
     highscoreMenu.style.display = 'none';
-    instructionsMenu.style.display = 'none';
 })
 
 // ==================== SCOREBOARD =============================
